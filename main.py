2177c46d5a1612f7af2556d09ff165bc658cd5f5
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2021-08-14 21:11:05 +0400

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-#Simple Onclick SpamBot
+# reading opening writing in the file and  filtering string the way I want
 
-import pyautogui
-import time
 
-for _ in range (30):#how many times needed to be written
-    pyautogui.typewrite("YOU'VE BEEN HACKED")#message
-    pyautogui.press("enter")#button press
-    time.sleep(0.5) #time
+import os
+
+p = []
+with open("s.txt", "r+") as file:
+    for line in file:
+        done = os.path.basename("r" + line)
+        a = done.replace('.', '')[:-5]
+        # format in the template I need
+        print("""{{title:"{}",description: "ინფორმაცია",output: "ფასი"}},""".format(a))
+        splitted = a.split("№")
+
+        p.append(splitted[0])
+
+    # remove dublicates
+    res = []
+    for i in p:
+        if i not in res:
+            res.append(i)
+    #
+    # second part:button making
+
+    for g in res:
+        # for j in range(len(res)):
+        print("""{{id : 1, name :"{}"}},""".format(g))
+
